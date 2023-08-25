5a9428c36b8a1757b55569633fdde81d45238a89
Pratyush sharma <125113219+Pratyushs411@users.noreply.github.com>
2023-08-26 03:27:25 +0530

    Add files via upload

---
+++ binarySearch.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int binarySearch(int arr[],int size,int key)
+{
+	int start = 0;
+	int end = size-1;
+	while(start<=end)
+	{
+		int mid = (start+end)/2;
+		if(arr[mid]==key)
+		{
+			return mid;
+		}
+		else if(arr[mid]>key)
+		{
+			end = mid-1;
+		}
+		else{
+			start = mid+1;
+		}
+	}
+	return -1;
+}
+int main()
+{
+	int n;
+	cout<<"enter lenght of array:";
+	cin>>n;
+	int arr[n];
+	cout<<"enter array:"<<endl;
+	for(int i = 0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	int a;
+	cout<<"enter number to find:";
+	cin>>a;
+	int result = binarySearch(arr,n,a);
+	if(result == -1)
+	{
+		cout<<"number not found"<<endl;
+		return 0;
+	}
+	cout<<"number is at index "<<result<<endl;
+	return 0;
+}
